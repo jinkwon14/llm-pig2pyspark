d4b1d5416f6f5013ae7e26cac2188b5d096b352e
Jin Kwon <jinkwon0425@gmail.com>
2024-05-06 22:14:15 -0700

    Add gitignore

--- converter.py
+++
@@ -1,7 +0,0 @@
-class Converter:
-    def __init__(self):
-        pass
-
-    def convert_to_pyspark(self, pig_code):
-        # Use LLM to convert PIG code to PySpark code
-        pass
--- output_handler.py
+++
@@ -1,7 +0,0 @@
-class OutputHandler:
-    def __init__(self):
-        pass
-
-    def output_pyspark_code(self, pyspark_code):
-        # Output PySpark code to the user interface
-        pass
